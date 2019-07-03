69ff8c7b1030bbfc0eb800c6ca2980e984e36f39
Moulid Ahmed <moulidah@buffalo.edu>
2019-07-03 19:12:04 -0400

    update

--- serverTest.py
+++ serverTest.py
@@ -1,3 +1,23 @@
 import discord
+from discord.ext import commands
+from birthday import calcAge, parsedate
+from collections import defaultdict
 
-token =
+token = 'NTk2MDEyMjMxNjM3MDczOTIx.XRzWmw.jLuhagDih7ujgXFg_mcceaJiTxI'
+bot = commands.Bot(command_prefix='!')
+
+datastore = defaultdict(dict)
+#for i in b.keys():
+#    await ctx.send(b[i]) ---> 21
+#{serverid : {user:{'birthday': age}}}   b=d[sID][user] ''
+@bot.command(alias='mybday','mybirthday')
+# !mybday or !mybirthday March 2, 1998
+async def mybirthday(ctx,*,arg):
+
+
+
+@bot.command(alias='birthday','bday')
+async def birthday(ctx, arg:discord.Member):
+     # !bday or !birthday @user ---> March 2, 1998
+     for i in arg:
+         ctx.send('{i.display_name} birthday is on {d[i]}')
