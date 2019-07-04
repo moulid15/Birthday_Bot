62b1a49e8a6482495895dcd6193af18360e99305
Moulid Ahmed <moulidah@buffalo.edu>
2019-07-03 21:53:06 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Birthday_Bot
 Birthday bot
+
+https://discordapp.com/api/oauth2/authorize?client_id=596012231637073921&permissions=8&scope=bot
--- serverTest.py
+++ serverTest.py
@@ -2,22 +2,45 @@ import discord
 from discord.ext import commands
 from birthday import calcAge, parsedate
 from collections import defaultdict
+from datetime import date
+import datetime
+import time
 
 token = 'NTk2MDEyMjMxNjM3MDczOTIx.XRzWmw.jLuhagDih7ujgXFg_mcceaJiTxI'
 bot = commands.Bot(command_prefix='!')
 
 datastore = defaultdict(dict)
-#for i in b.keys():
-#    await ctx.send(b[i]) ---> 21
-#{serverid : {user:{'birthday': age}}}   b=d[sID][user] ''
-@bot.command(alias='mybday','mybirthday')
+
+#for i in d.keys():
+#    await ctx.send(d[i]) ---> 21
+#{serverid : {user:{'birthday': age}}}   d=datastore[server][i]
+@bot.command(alias='mybday')
 # !mybday or !mybirthday March 2, 1998
 async def mybirthday(ctx,*,arg):
+    message = ctx.message
+    print('adding birthday data....')
+    date = parsedate((arg))
+    age = calcAge(datetime.date(date[-1],date[-3],date[-2]))
+    if message.guild.id not in datastore:
+        datastore[message.guild.id]={message.author:{arg:age}}
+    else:
+        datastore[message.guild.id][message.author]={arg:age}
+    print(datastore)
+    d =datastore[message.guild.id][message.author]
+    for i in d.keys():
+        await ctx.send(d[i])
 
 
 
-@bot.command(alias='birthday','bday')
-async def birthday(ctx, arg:discord.Member):
+@bot.command(alias='birthday1')
+async def birthday(ctx, *arg:discord.Member):
      # !bday or !birthday @user ---> March 2, 1998
+     id = ctx.message.guild.id
      for i in arg:
-         ctx.send('{i.display_name} birthday is on {d[i]}')
+         for j in datastore[id][i]:
+             await ctx.send(str(i.display_name)+' birthday is on '+ str(j))
+@bot.command()
+async def count(ctx):
+    await ctx.send("yo")
+
+bot.run(token)
