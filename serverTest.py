8a07c30829313b02d4f21ecf7f0de32f9fa008b2
Moulid Ahmed <moulidah@buffalo.edu>
2019-07-03 21:59:07 -0400

    moulid

--- serverTest.py
+++ serverTest.py
@@ -10,7 +10,6 @@ token = 'NTk2MDEyMjMxNjM3MDczOTIx.XRzWmw.jLuhagDih7ujgXFg_mcceaJiTxI'
 bot = commands.Bot(command_prefix='!')
 
 datastore = defaultdict(dict)
-<<<<<<< HEAD
 
 #for i in d.keys():
 #    await ctx.send(d[i]) ---> 21
@@ -32,7 +31,6 @@ async def mybirthday(ctx,*,arg):
         await ctx.send(d[i])
 
 
-
 @bot.command(alias='birthday1')
 async def birthday(ctx, *arg:discord.Member):
      # !bday or !birthday @user ---> March 2, 1998
@@ -40,27 +38,11 @@ async def birthday(ctx, *arg:discord.Member):
      for i in arg:
          for j in datastore[id][i]:
              await ctx.send(str(i.display_name)+' birthday is on '+ str(j))
-@bot.command()
-async def count(ctx):
-    await ctx.send("yo")
-
-bot.run(token)
-=======
-#for i in b.keys():
-#    await ctx.send(b[i]) ---> 21
-# DIC -:  {serverid : {user:{'birthday': age}}}   b=d[sID][user] ''
-# @bot.command(alias='mybday','mybirthday')
-# # !mybday or !mybirthday March 2, 1998
-# async def mybirthday(ctx,*,arg):
-
-
-# !age -> get user age
 @bot.command(alias='age')
 async def userAge(ctx,*arg:discord.Member):
     msg = ctx.message
     server = msg.guild
     for i in arg:
-        for j in datastore[server][i].keys()):
+        for j in datastore[server][i].keys():
             await ctx.send(str(i.display_name) + " is "+str(j)+ " years old");
-bot.run(token);
->>>>>>> 78637c8d0fb8c12026e4fe5b560fadd02e6d7dbf
+bot.run(token)
